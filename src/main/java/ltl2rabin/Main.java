f1e7d887562ccd089be88db32e944d0cba13d0f0
Hannes Bibel <hbibel@users.noreply.github.com>
2015-02-26 11:50:27 +0100

    Initial commit with gradle build file and minimal project structure

---
+++ src/main/java/ltl2rabin/Main.java
@@ -0,0 +1,12 @@
+package ltl2rabin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+    }
+
+    public static int add(int a, int b)  {
+        return a + b;
+    }
+}
