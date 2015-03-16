767a59bd5321be2e26fed18147348826b84ac301
Hannes Bibel <hbibel@users.noreply.github.com>
2015-03-16 18:18:50 +0100

    Optimized reach() uses queue and now also identifies sinks;

--- src/main/java/ltl2rabin/MojmirAutomaton.java
+++ src/main/java/ltl2rabin/MojmirAutomaton.java
@@ -3,7 +3,9 @@ package ltl2rabin;
 import com.google.common.collect.Sets;
 
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.BiFunction;
 
 /**
@@ -13,38 +15,65 @@ import java.util.function.BiFunction;
  */
 public class MojmirAutomaton<T, U> {
     public Set<State> acceptingStates; // yet to be identified
-    public Set<State> sinks; // yet to be identified
-    public HashSet<State> states;
-    public HashSet<U> alphabet;
+    public Set<State> sinks;
+    public Set<State> states;
+    public Set<U> alphabet;
     public BiFunction<T, Set<U>, T> transitionFunction;
 
     public MojmirAutomaton(T info, BiFunction<T, Set<U>, T> transitionFunction, HashSet<U> alphabet) {
         this.alphabet = alphabet;
         states = new HashSet<State> ();
-        states.add(new State(info));
+        State firstState = new State(info);
+        firstState.setOrigin(true);
+        states.add(firstState);
+        sinks = new HashSet<>();
         this.transitionFunction = transitionFunction;
         reach();
     }
 
     public void reach() {
-        HashSet<State> statesToBeAdded = new HashSet<State> ();
+        Queue<State> statesToBeAdded = new ConcurrentLinkedQueue<State>(states);
         Set<Set<U>> words = Sets.powerSet(alphabet);
 
-        for (State state : states) {
+        while (!statesToBeAdded.isEmpty()) {
+            State temp = statesToBeAdded.poll();
+            boolean isSink = true;
             for (Set<U> word : words) {
-                T newStateInfo = transitionFunction.apply(state.info, word);
+                T newStateInfo = transitionFunction.apply(temp.info, word);
+                if (newStateInfo.equals(temp.info)) continue;
+                isSink = false;
                 State newState = new State(newStateInfo);
-                boolean debugCheck = statesToBeAdded.add(newState);
+                if (!states.add(newState)) continue; // Remark: states is a set, so no duplicate states will be added, instead false is returned
+                statesToBeAdded.offer(newState);
+            }
+            if (isSink && (!temp.isOrigin())) {
+                temp.setSink(true);
+                sinks.add(temp);
             }
         }
-        if (states.addAll(statesToBeAdded)) reach(); // if new states have been added, repeat
     }
 
     public class State {
         T info;
+        boolean isSink;
+        boolean isOrigin;
 
         public State(T info) {
+            isSink = false;
+            isOrigin = false;
             this.info = info;
+        }
+
+        public void setSink(boolean isSink) {
+            this.isSink = isSink;
+        }
+
+        public void setOrigin(boolean isOrigin) {
+            this.isOrigin = isOrigin;
+        }
+
+        public boolean isOrigin() {
+            return isOrigin;
         }
 
         @Override
