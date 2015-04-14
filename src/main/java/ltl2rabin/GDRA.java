0e9523448b31323eeadc13f0cf8a9266a450d6d0
Hannes Bibel <hannes.bibel@gmail.com>
2015-04-14 12:53:11 +0200

    Added GDRA

---
+++ src/main/java/ltl2rabin/GDRA.java
@@ -0,0 +1,74 @@
+package ltl2rabin;
+
+import com.google.common.collect.Sets;
+import org.apache.commons.collections4.set.ListOrderedSet;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class GDRA<T, U> {
+    private State initialState;
+    private ListOrderedSet<State> states = new ListOrderedSet<>();
+    private int stateCounter = 0; // Remove in final version
+
+    public GDRA(Set<U> alphabet, MojmirAutomaton<T, U> mojmirAutomaton, ProductAutomaton<T, U> productAutomaton) {
+        MojmirAutomaton<T, U>.State initialMojmirState = mojmirAutomaton.getInitialState();
+        ProductAutomaton<T, U>.State initialPAState = productAutomaton.getInitialState();
+        initialState = new State(initialMojmirState, initialPAState);
+
+        Queue<State> stateQueue = new ConcurrentLinkedQueue<>();
+        stateQueue.add(initialState);
+        Set<Set<U>> letters = Sets.powerSet(alphabet);
+        while (!stateQueue.isEmpty()) {
+            State tempState = stateQueue.poll();
+            for (Set<U> letter : letters) {
+                MojmirAutomaton<T, U>.State nextMojmirState = tempState.mojmirState.readLetter(letter);
+                ProductAutomaton<T, U>.State nextPAState = tempState.paState.readLetter(letter);
+
+                State newState = new State(nextMojmirState, nextPAState);
+                int index = states.indexOf(newState);
+                if (index >= 0) {
+                    newState = states.get(index);
+                    stateCounter--;
+                }
+                else {
+                    states.add(newState);
+                }
+                tempState.setTransition(letter, newState);
+                if (newState.transitions.size() < letters.size()) {
+                    stateQueue.add(newState);
+                }
+            }
+        }
+    }
+
+    public class State {
+        MojmirAutomaton<T, U>.State mojmirState;
+        ProductAutomaton<T, U>.State paState;
+        HashMap<Set<U>, State> transitions;
+        String label = "gq" + stateCounter;
+
+        public State(MojmirAutomaton<T, U>.State mojmirState, ProductAutomaton<T, U>.State paState) {
+            this.mojmirState = mojmirState;
+            this.paState = paState;
+        }
+
+        public void setTransition(Set<U> letter, State to) {
+            transitions.put(letter, to);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            State state = (State) o;
+            return Objects.equals(mojmirState, state.mojmirState) &&
+                    Objects.equals(paState, state.paState);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(mojmirState, paState);
+        }
+    }
+}
