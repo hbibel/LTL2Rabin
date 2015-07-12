4cfc9a1f69d935e26f331e34ae5551e820f0a57c
hbibel <hbibel@users.noreply.github.com>
2015-07-12 22:06:05 +0200

    Updated Version number -> 0.9

--- src/main/java/ltl2rabin/Main.java
+++ src/main/java/ltl2rabin/Main.java
@@ -1,81 +1,78 @@
 package ltl2rabin;
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @author Hannes Bibel
+ *
+ */
+
 import ltl2rabin.LTL.*;
-import org.apache.commons.cli.*;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         PropEquivalenceClass.suppressBDDOutput(); // keep System.out clean from messages by the BDD library.
+        /* Right now, this program parses the input args into an ArrayList. I strongly recommend using the
+         * org.apache.commons.cli package since it follows the POSIX Program Argument Syntax Conventions and also it
+         * makes life easier. Unfortunately, it is not compatible with the argument format used by Rabinizer 3.1,
+         * so for now we stick to handling arguments as a List. */
+        List<String> argList = Arrays.asList(args);
 
-        Options options = new Options();
-        options.addOption("nofile", false, "Don't read the LTL formula from a file, instead enter it in the console (via stdin).\n");
-        Option formulaOption = Option.builder("f").argName("formula")
-                                               .longOpt("formula")
-                                               .hasArgs()
-                                               .desc("Read the LTL formula as command line argument.").build();
-        options.addOption(formulaOption);
-        options.addOption("h", "help", false, "Display help message.");
+        String helpMessage = "usage: java -jar LTL2Rabin.jar [options] [<filename>/<formula>]\n" +
+                " -h,--help     Display this help message.\n" +
+                " -v,--version  Display the number of this version of LTL2Rabin\n" +
+                " -in=file      LTL2Rabin will read the formula from a file. Don't forget \n" +
+                                "               " +
+                                "to specify a file name. Multiple formulas per file are not \n" +
+                                "               " +
+                                "supported.\n" +
+                " -out=std      The resulting automaton will be printed to the terminal\n";
 
-        if (0 == args.length) {
-            printHelp(options);
+        if (argList.contains("-h") || argList.contains("--help") || 0 == args.length) {
+            System.out.println(helpMessage);
             return;
         }
 
-        CommandLineParser parser = new DefaultParser();
+        if (argList.contains("--version") || argList.contains("-v")) {
+            System.out.println("This is LTL2Rabin version 0.9\n");
+        }
 
+        boolean fileOutput = true;
+        if (argList.remove("-out=std")) {
+            fileOutput = false;
+        }
+
+        LTLFactory.Result parserResult = null;
         String inputString = "";
         String outputFilename = "ltl2rabin.hoa";
-        LTLFactory.Result parserResult = null;
-        CommandLine cmdLine;
-        List<String> remainingArguments; // The arguments that don't have a corresponding object of the Options type
-        try {
-            cmdLine = parser.parse(options, args);
-        }
-        catch (ParseException e) {
-            System.err.println("Command line arguments parsing failed. Reason: " + e.getMessage());
-            printHelp(options);
-            return;
-        }
-
-        remainingArguments = cmdLine.getArgList();
-        if (cmdLine.hasOption("h") || cmdLine.hasOption("help")) {
-            printHelp(options);
-            return;
-        }
-        else if (cmdLine.hasOption("nofile")) {
-            System.out.println("Please enter a valid LTL formula:"); // TODO: Print LTL rules
-            try {
-                inputString = new BufferedReader(new InputStreamReader(System.in)).readLine();
-            } catch (IOException e) {
-                System.err.println("An error occured when reading the input. Reason: " + e.getMessage());
-                e.printStackTrace();
-            }
-        }
-        else if (cmdLine.hasOption("formula")) {
-            inputString = String.join(" ", Arrays.asList(cmdLine.getOptionValues("formula")));
-            System.out.println("Okay, I will generate an automaton for this formula: " + inputString);
-        }
-
-        if (0 == inputString.length()) {
-            if (0 == remainingArguments.size()) {
-                System.out.println("No file name detected. Please specify a name for the input file or run the " +
-                        "program again with the -nofile argument.");
-                printHelp(options);
-                return;
-            }
-            // Input via file
-            // For now, only one file gets read per run.
-            String fileName = remainingArguments.get(0);
+        if (argList.remove("-in=file")) {
+            // File input
+            String fileName = argList.get(argList.size() - 1);
             outputFilename = fileName + ".hoa";
-            parserResult = new LTLFactoryFromFile().buildLTL(new File(fileName));
+            File inputFile = new File(fileName);
+            if (inputFile.exists() && !inputFile.isDirectory()) {
+                parserResult = new LTLFactoryFromFile().buildLTL(inputFile);
+            }
+            else {
+                System.out.println("Sorry, I could not find a file named " + fileName + "\n");
+                System.out.println(helpMessage);
+            }
         }
         else {
-            // String input
+            // Parse input formula from args
+            // If there are additional arguments starting with '-', just ignore them.
+            inputString = String.join(" ", argList.stream().filter(s -> s.charAt(0) != '-').collect(Collectors.toList()));
+            System.out.println("Okay, I will generate an automaton for this formula: " + inputString + "\n");
             parserResult = new LTLFactoryFromString().buildLTL(inputString);
         }
 
@@ -84,23 +81,24 @@ public class Main {
 
             GDRAFactory factory = new GDRAFactory(parserResult.getAlphabet());
             GDRA gdra = factory.createFrom(parserResult);
-            try {
-                PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputFilename)));
-                out.print(HanoiFormatStringFactory.toHOAFv1(gdra));
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
             long stopTime = System.currentTimeMillis();
             long elapsedTime = stopTime - startTime;
-            System.out.println(parserResult.getLtlFormula() + " Finished in " + elapsedTime + "ms " +
-                    "(=" + elapsedTime / 1000 / 60 + "m " + (elapsedTime % 60000) / 1000 + "s " + (elapsedTime % 1000) +"ms)");
-        }
-    }
+            System.out.println("Construction of GDRA for " + parserResult.getLtlFormula() + " finished in " + elapsedTime + "ms " +
+                    "(=" + elapsedTime / 1000 / 60 + "m " + (elapsedTime % 60000) / 1000 + "s " + (elapsedTime % 1000) +"ms)\n\n");
 
-    private static void printHelp(Options options) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("java -jar LTL2Rabin.jar [options] [<filename>]", options);
+            if (fileOutput) {
+                try {
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputFilename)));
+                    out.print(HanoiFormatStringFactory.toHOAFv1(gdra));
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                System.out.println(HanoiFormatStringFactory.toHOAFv1(gdra));
+            }
+        }
     }
 }
